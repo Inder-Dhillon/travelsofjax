4d8322ac18ebc65edf097a98c2b8ec859650267b
Inderpal Singh Dhillon <36529232+Inder-Dhillon@users.noreply.github.com>
2019-03-26 17:04:49 -0400

    Actively monitors #jaxson now

--- jax.py
+++ jax.py
@@ -14,3 +14,38 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
+class StdOutListener(StreamListener):
+
+    def on_status(self, status):
+        if 'RT' not in status.text:
+            print('Tweet text: ' + status.text)
+            s = status.author.screen_name
+            print(s)
+            api.create_favorite(status.id)
+            try:
+                api.retweet(status.id)
+                message = '@' + s + ' You tweeted with my hashtag!'
+                api.update_status(status=message, in_reply_to_status_id=status.id)
+            except:
+                try:
+                    print('duplicate tweet error')
+                    message = '@' + s + ' You tweeted with my hashtag AGAIN!'
+                    api.update_status(status=message, in_reply_to_status_id=status.id)
+                except:
+                    print('double duplicate tweet error')
+        return True
+
+    def on_error(self, status_code):
+        print('Got an error with status code: ' + str(status_code))
+        return True  # To continue listening
+
+    def on_timeout(self):
+        print('Timeout...')
+        return True  # To continue listening
+
+
+if __name__ == '__main__':
+    listener = StdOutListener()
+
+    stream = Stream(auth, listener)
+    stream.filter(track=['#jaxson'])
